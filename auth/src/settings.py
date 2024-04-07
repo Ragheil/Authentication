b471a268c00ee246570fdd47a0493742f2c5f960
Ragheil <Ragheil123@gmail.com>
2024-04-07 22:30:23 +0700

    Settings mod

--- auth/src/settings.py
+++ auth/src/settings.py
@@ -125,6 +125,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'atacadorragheil@gmail.com'
-EMAIL_HOST_PASSWORD = 'cpxxllvjuybtmnyx'
+EMAIL_HOST_USER = 'atacadorragheil@gmail.com' #akoa ning API , PLEASE DONT USE IT FOR BOLD HEHE
+EMAIL_HOST_PASSWORD = 'cpxxllvjuybtmnyx' #akoa ning pass sa gmail , PLEASE DONT USE IT FOR BOLD HEHE
 
